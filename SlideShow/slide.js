064302b109919577bedf798a29f5f7433074cdb2
weslley-batista <wbh@cin.ufpe.br>
2022-03-06 20:30:41 -0300

    Carrosel
    2 carrossel, automático e que é alterado por botões

--- SlideShow/slide.js
+++ SlideShow/slide.js
@@ -12,5 +12,29 @@ function carrosel( ){
     
     images.style.transform = `translatex(${-idx * 500}px)`; //muda o eixo X em 500px
 };
+setInterval(carrosel,2000);
 
-setInterval(carrosel,1800); 
+/*Carrosel com botoes*/
+const images1 = document.getElementById("img-carrosel-botao");
+const image1 = document.querySelectorAll("#img-carrosel-botao img");
+
+const anterior = document.getElementById("anterior");
+const proximo = document.getElementById("proximo");
+
+function nextImg(){
+    idx++;
+    if(idx > image1.length-1){
+        idx = 0;
+    }
+    images1.style.transform = `translatex(${-idx * 500}px)`; //muda o eixo X em 500px
+
+}
+function previousImg(){
+    idx--;
+    if(idx < 0){
+        idx = image1.length-1;
+    }
+    images1.style.transform = `translatex(${-idx * 500}px)`; //muda o eixo X em 500px
+}
+proximo.addEventListener('click', nextImg);
+anterior.addEventListener('click',previousImg);
